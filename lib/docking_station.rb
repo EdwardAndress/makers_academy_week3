44bd8b0629815bd139e5c9dcec1a54a7aac7fe80
edward andress <eddie_andress@hotmail.com>
2014-10-02 16:30:55 +0100

    added release method for hiring a bike

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,4 +4,11 @@ class DockingStation #Is the point from which people can hire bikes
 
 include BikeContainer
 
+
+	def release_bike_for_hire
+		if self.working_bikes.any?
+			bikes.delete(self.working_bikes.shift)
+		end
+	end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,7 +4,16 @@ require './lib/docking_station'
 
 describe DockingStation do
 
-	it_behaves_like 'a bike container'	
+	it_behaves_like 'a bike container'
+
+	context 'when being used by a person' do
+		it 'can release a working bike for hire' do
+			station = DockingStation.new(bikes: [Bike.new, Bike.new])
+			expect(station.bikes.count).to eq 2
+			station.release_bike_for_hire
+			expect(station.bikes.count).to eq 1
+		end
+	end	
 
 end
 
