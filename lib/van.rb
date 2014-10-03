56594d28fc5f6b66df8a064942bed2b279b9973c
edward andress <eddie_andress@hotmail.com>
2014-10-03 17:07:26 +0100

    fixed bug with van not receiving broken bikes after release from docking station

--- lib/van.rb
+++ lib/van.rb
@@ -1,11 +1,12 @@
 require_relative './bike_container'
 
-class Van
+class Van #used to collect broken bikes from dockingstations, deliver them to the garage and redistribute working bikes
 
 include BikeContainer
 
 	def collect_broken_bikes_from(station)
-		station.non_customer_release(broken_bikes)
+		station.broken_bikes.each {|bike| self.dock(bike)} #loads each broken bike into the van
+		station.non_customer_release(station.broken_bikes) #deletes all the broken bikes from the docking station
 	end
 
 end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -3,14 +3,17 @@ require 'van'
 
 describe Van do
 
-	let(:station) {DockingStation.new}
-	let(:van) {Van.new}
+	let(:bike)						{double :bike, :broken? => false}
+	let(:broken_bike)			{double :bike, :broken? => true}
+	let(:station) 				{DockingStation.new(bikes: [broken_bike, bike])}
+	let(:van) 						{Van.new}
 
 	it_behaves_like 'a bike container'
 
 	it 'can collect broken bikes from a station' do
 		expect(station).to receive(:non_customer_release).with(station.broken_bikes)
 		van.collect_broken_bikes_from(station)
+		expect(van.bikes.count).to eq 1
 	end
 
 end
