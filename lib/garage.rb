553e63f9ebb4b56dc44ea2711400d235bb3da31e
edward andress <eddie_andress@hotmail.com>
2014-10-04 11:05:34 +0100

    garage receives broken bikes from van

--- lib/garage.rb
+++ lib/garage.rb
@@ -4,4 +4,8 @@ class Garage #used to fix broken bikes
 
 	include BikeContainer
 
+	def remove_broken_bikes_from(van)
+		self.bikes << (van.non_customer_release(van.broken_bikes))
+	end
+
 end
--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -3,16 +3,18 @@ require 'garage'
 describe Garage do
 
   let(:garage)          {Garage.new}
-  let(:van)             {double :van}
-  let(:broken_bike)     {double :bike, :broken => true}
-  let(:working_bike)    {double :bike, :broken => false}
-  let(:fixed_bike)      {double :bike, :broken => false}  
+  let(:van)             {double :van, :broken_bikes => broken_bike, :non_customer_release => broken_bike}
+  let(:broken_bike)     {double :bike, :broken? => true}
+  let(:working_bike)    {double :bike, :broken? => false}
+  let(:fixed_bike)      {double :bike, :broken? => false}  
 
   it_behaves_like 'a bike container'
 
   context 'when receiving bikes' do 
 
     it 'accepts broken bikes from a van' do
+      garage.remove_broken_bikes_from(van)
+      expect(garage.broken_bikes.count).to eq 1
     end
 
     it 'fixes broken bikes' do
