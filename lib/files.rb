db9a6377e93ddd584ef052da93525f4dee2480e9
edward andress <eddie_andress@hotmail.com>
2014-10-06 12:09:11 +0100

    refactoring domain_summary method

--- lib/files.rb
+++ lib/files.rb
@@ -7,22 +7,39 @@ require_relative './bike_container'
 require_relative './van'
 require_relative './garage'
 
+
+def domain_summary
+	available_objects.each {|object| section_header(object) ; object_methods(object)}
+	module_summary(BikeContainer)
+end
+
 def available_objects #top level method which shows the user what objects can be instatiated as part of this domain
 	[Person, Bike, Van, DockingStation, Garage]
 end
 
-def objects_and_methods
-	available_objects.each do |object|
-		puts '=' * 20
-		print object.new.class ; puts ' methods'
-		puts '-' * 20
-		object.instance_methods(false).each {|method| puts method}
-		puts'=' * 20
-	end
+def module_summary(mod)
+	section_start_and_end
 	puts "Methods shared by all objects that can contain bikes (Vans, DockingStations and Garages)"
+	header_end
+	mod.instance_methods(false).each {|method| puts method}
+	section_start_and_end
+end
+
+def section_header(object)
+	section_start_and_end
+	print object.new.class ; puts ' methods'
+	header_end
+end
+
+def object_methods(object)
+	object.instance_methods(false).each {|method| puts method}
+	section_start_and_end
+end
+
+def header_end
 	puts '-' * 20
-	BikeContainer.instance_methods(false).each do |method|
-		puts method
-	end
+end
+
+def section_start_and_end
 	puts '=' * 20
 end
